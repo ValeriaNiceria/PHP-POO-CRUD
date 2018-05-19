d68c9c4cb20794b4469df1a4ea12b1c5f54c9001
ValeriaNiceria <valerianiceria@gmail.com>
2018-05-18 22:10:32 -0300

    Conexão

---
+++ class/DbConfig.php
@@ -0,0 +1,25 @@
+<?php
+
+class DbConfig {
+	private $host = 'localhost';
+	private $database = 'php_oop';
+	private $user = 'root';
+	private $password = '54321';
+
+	protected $connection;
+
+	public function __construct() {
+		if (!isset($this->connection)) {
+			$this->connection = new mysqli($this->host, $this->user, $this->password, $this->database);
+			if (!$this->connection) {
+				echo 'Erro ao conectar!';
+				exit;
+			}
+		}
+		return $this->connection;
+	}
+
+
+}
+
+?>
